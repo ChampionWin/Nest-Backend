2ea90aafb01dbdc66cb84a4fbae74c4a1dd263db
ChampionWin <championwillwin6@outlook.com>
2020-09-21 06:32:24 -0400

    Added new controller

---
+++ src/products/products.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body } from '@nestjs/common';
+
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ): any {
+    const generateId = this.productService.insertProduct(
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    return { id: generateId };
+  }
+}
